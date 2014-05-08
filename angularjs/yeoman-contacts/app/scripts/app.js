138bfe169125d9138c66f4337218333813dab74f
Clauber Stipkovic <clauber.halic@gmail.com>
2014-05-08 19:27:12 -0300

    criacao da estrutura do projeto e do route home

---
+++ angularjs/yeoman-contacts/app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular
+  .module('yeomanContactsAppApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ angularjs/yeoman-contacts/app/scripts/controllers/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('yeomanContactsAppApp')
+  .controller('HomeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
