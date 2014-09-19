2f7d2c05611e5d3d125ab1805e256d59c415b329
Clauber Stipkovic <clauber.halic@gmail.com>
2014-09-19 19:46:51 -0300

    hello word

---
+++ objective-c/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Clauber Stipkovic on 9/19/14.
+//  Copyright (c) 2014 CS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
