63e77c2e388d60ebf2e393627feff65d72b16944
Clauber Stipkovic <clauber.halic@gmail.com>
2014-09-30 01:01:31 -0300

    fraction in objective-c

--- objective-c/Fraction/Fraction/main.m
+++ objective-c/Fraction/Fraction/main.m
@@ -12,6 +12,21 @@
 int main(int argc, const char * argv[]) {
     
     Fraction *f1 = [[Fraction alloc] init];
+    Fraction *f2= [[Fraction alloc] init];
+    
+    f1.numerator = 2;
+    f1.denominator = 3;
+    
+    [f2 setNumerator:3 andDenominator:4];
+    
+    [f1 print];
+    [f2 print];
+    
+    [f1 multiply:f2];
+    
+    [f1 print];
+    
+    NSLog(@"F1 * F2 = %.lf", [f1 toDouble]);
     
     return NSApplicationMain(argc, argv);
 }
