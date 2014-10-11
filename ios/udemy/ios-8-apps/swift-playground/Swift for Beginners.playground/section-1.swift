d5fb60d6653b45b8d83003226ff9e6c0628a591e
Clauber Stipkovic <clauber.halic@gmail.com>
2014-10-11 14:14:09 -0300

    swift playground

--- ios/udemy/ios-8-apps/swift-playground/Swift for Beginners.playground/section-1.swift
+++ ios/udemy/ios-8-apps/swift-playground/Swift for Beginners.playground/section-1.swift
@@ -2,8 +2,31 @@
 
 import UIKit
 
+// Aula5
+
 var str = "Hello, playground"
 
 let π = 3.14
-var x : String
-x = "String var"
+var string : String
+string = "String var"
+
+
+// Aula6
+
+var int : Int
+int = 1
+
+var f: Float
+f = 10.2
+
+var d : Double
+d = 10.23232132
+
+var zeroO = 0x4f
+
+typealias FileSize = UInt
+
+var fsize : FileSize
+
+// Aula 7
+
