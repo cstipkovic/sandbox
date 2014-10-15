93bdd69a4fb971564edcb3e57551d8dde79c2617
Clauber Stipkovic <clauber.halic@gmail.com>
2014-10-14 23:38:14 -0300

    switf classes

--- ios/udemy/ios-8-apps/swift-playground/Swift for Beginners.playground/section-1.swift
+++ ios/udemy/ios-8-apps/swift-playground/Swift for Beginners.playground/section-1.swift
@@ -30,3 +30,65 @@ var fsize : FileSize
 
 // Aula 7
 
+var isFileTooBig = true;
+
+if isFileTooBig {
+//    true 
+} else {
+//    false
+}
+
+// Aula 8
+
+var someTuple = (someNumber: 14, someString: "Hello World!");
+
+someTuple.someNumber;
+someTuple.0;
+
+// Aula 9
+
+var stringNumber = "64";
+let convertedNumber = stringNumber.toInt();
+
+if let constant = convertedNumber {
+//    if convertedNumber is a number
+} else {
+//    false
+}
+
+var number : Int?
+// ...
+number = 6
+number!
+
+// Aula 10
+
+var x = 5;
+var y = 10;
+
+x = y
+
+var (q, w) = (4, 8)
+
+4 + 3
+6 - 2
+5 * 5
+4 / 2
+
+"Hello " + "you"
+
+var h : Character = "H"
+var o : Character = "W"
+var hw = h + o
+
+11 % 4
+
+8.50 % 0.75
+
+// Aula 11
+
+var unary = 0
+++unary
+unary++ // shows the variable before increment
+++unary
+
