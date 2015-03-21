3b0f9b8c8b3189c0c036a199dcf1fc38ff008220
Clauber Stipkovic <clauber.halic@gmail.com>
2015-03-21 01:51:38 -0300

    ios with xamarin and typescript

---
+++ ios/XamarinFA/XamarinFApp/XamarinFAppViewController.designer.cs
@@ -0,0 +1,25 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamarinFApp
+{
+	[Register ("XamarinFAppViewController")]
+	partial class XamarinFAppViewController
+	{
+		[Action ("teste:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void teste (UITextField sender);
+
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
---
+++ typescript/greeter.ts
@@ -0,0 +1,7 @@
+function greeter(person: list) {
+	return "Hello, " + person;
+}
+
+var user = [0, 1, 2];
+
+document.body.innerHTML = greeter(user);
