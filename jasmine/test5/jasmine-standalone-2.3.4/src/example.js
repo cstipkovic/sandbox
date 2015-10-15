ebffa57f318297aa369c9e629334920e64770b61
Clauber Stipkovic <cin_chalic@uolinc.com>
2015-10-15 16:23:13 -0300

    test 4 and 5

---
+++ jasmine/test5/jasmine-standalone-2.3.4/src/example.js
@@ -0,0 +1,15 @@
+var someFunction = function () {
+    return true;
+};
+
+var anotherFunction = function () {
+    return ['teste', 'bla', 't'];
+};
+
+var User = {
+    age: 21,
+
+    getAge: function () {
+        return this.age;
+    }
+};
