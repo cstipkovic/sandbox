9c6bb4bf04fed8d1a6573af5d8564ca77721fc4e
Clauber Stipkovic <cin_chalic@uolinc.com>
2016-07-11 19:32:07 -0300

    express-mysql

---
+++ express-mysql/config/db_config.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'pass',
+    database: 'express-mysql'
+});
+
+connection.connect();
+
+connection.query('select * from table-name', function(err, rows, fields) {
+    if (!err) {
+        console.log('The solution is:', rows);
+    } else {
+        console.log('Error');
+    }
+});
+
+connection.end();
