ffb37e12593988bfebb5b81648bfe23818ed2efb
Clauber Stipkovic <clauber.halic@gmail.com>
2020-02-12 00:28:23 -0300

    Adding more tests for react js

--- react-udemy/components/src/index.js
+++ react-udemy/components/src/index.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from './CommentDetail';
 import Faker from 'faker';
+
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail 
-                avatar={ Faker.image.avatar() }
-                author={ Faker.name.firstName() }
-                timeAgo="Today at 4:00PM"
-                content={ Faker.lorem.text() }
-                
-            />
-            <CommentDetail 
-                avatar={ Faker.image.avatar() }
-                author={ Faker.name.firstName() }
-                timeAgo="Today at 5:00PM" 
-                content={ Faker.lorem.text() }
-            />
-            <CommentDetail 
-                avatar={ Faker.image.avatar() }
-                author={ Faker.name.firstName() }
-                timeAgo="Yesterday at 6:00PM" 
-                content={ Faker.lorem.text() }
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={ Faker.image.avatar() }
+                    author={ Faker.name.firstName() }
+                    timeAgo="Today at 4:00PM"
+                    content={ Faker.lorem.text() }
+                    
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={ Faker.image.avatar() }
+                    author={ Faker.name.firstName() }
+                    timeAgo="Today at 5:00PM" 
+                    content={ Faker.lorem.text() }
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={ Faker.image.avatar() }
+                    author={ Faker.name.firstName() }
+                    timeAgo="Yesterday at 6:00PM" 
+                    content={ Faker.lorem.text() }
+                />
+            </ApprovalCard>
         </div>
     );
 };
