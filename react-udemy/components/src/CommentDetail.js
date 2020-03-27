c4bc992365a0eb8b886fce7564a9b145e7ecea77
Clauber Stipkovic <clauber.halic@gmail.com>
2020-03-26 21:59:35 -0300

    Just fixing indentation

--- react-udemy/components/src/CommentDetail.js
+++ react-udemy/components/src/CommentDetail.js
@@ -13,7 +13,7 @@ const CommentDetail = (props) => {
                 <div className="metadata">
                     <span className="date">{ props.timeAgo }</span>
                 </div>
-                    <div className="text">{ props.content }</div>
+                <div className="text">{ props.content }</div>
             </div>
         </div>
     );
