8424a89817eb1a6fb3fa26a6a66782ce1de65912
Clauber Stipkovic <clauber.halic@gmail.com>
2021-03-18 23:07:43 -0300

    reactjs component seasons

--- react-udemy/seasons/src/SeasonDisplay.js
+++ react-udemy/seasons/src/SeasonDisplay.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
 
-const SesasonDisplay = () => {
-    return <div>Season Display</div>;
+const seasonConfig = {
+  summer: {
+    text: "Hot",
+    iconName: "sun",
+  },
+  winter: {
+    text: "Cold",
+    iconName: "snowflake",
+  },
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat > 0 ? "winter" : "summer";
+  }
+};
+
+const SesasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <>
+      <i className={`${iconName} icon`} />
+      {text}
+    </>
+  );
 };
 
 export default SesasonDisplay;
--- react-udemy/seasons/src/index.js
+++ react-udemy/seasons/src/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
-    return <div>Hey!</div>
-};
+import SesasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <>Error: {this.state.errorMessage}</>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SesasonDisplay lat={this.state.lat} />;
+    }
+
+    return <>Loading...</>;
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
