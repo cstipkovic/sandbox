4e2abef292400100de5591fb92a70b95add5a2c1
Clauber Stipkovic <clauber.halic@gmail.com>
2022-10-16 16:12:59 -0300

    Updating URLSearchParams and removing queryparams

--- node/test/app.js
+++ node/test/app.js
@@ -1,11 +1,19 @@
-var http = require('http');
-var querystring = require('querystring');
+import http from "http";
 
-http.createServer(function(req, res) {
-	var qs = querystring.parse(req.url.split('?')[1]);
-	var userName = qs.firstName + ' ' + qs.lastName;
-	var html = '<!doctype html>' + '<html><head><title>Hello ' + userName + '</title></head>' +
-		'<body><h1>Hello, ' + userName + '!</h1></body></html>';
+http
+  .createServer(function (req, res) {
+    const qs = new URLSearchParams(req.url.split("?")[1]);
 
-	res.end(html);
-}).listen(8000);
+    const userName = qs.get("firstName") + " " + qs.get("lastName");
+    const html =
+      "<!doctype html>" +
+      "<html><head><title>Hello " +
+      userName +
+      "</title></head>" +
+      "<body><h1>Hello, " +
+      userName +
+      "!</h1></body></html>";
+
+    res.end(html);
+  })
+  .listen(8000);
